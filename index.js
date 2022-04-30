01b0ad962b141b9bca7a759479058b640598d22c
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-01 02:31:14 +0600

    server side is runnig

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+//require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome To Bookhouse!!!");
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
