eba54261585f8224270670ccf1253973f3b9c5b8
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-01 02:58:50 +0600

    db connected

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const cors = require("cors");
-//require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.haogd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const bookCollections = client.db("bookhouse").collection("book");
+   
+
+
+    console.log("Connected correctly to server");
+    
+  }
+  finally {
+
+  }
+}
+run().catch(console.dir);
+
 
 
 
