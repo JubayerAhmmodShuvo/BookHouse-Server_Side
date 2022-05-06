e2443fba6aa2dd9ed0162347df4492b0ff97c1d5
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-07 01:41:57 +0600

    deleted worked fine

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function run() {
       res.send(result);
     })
 
-    app.delete('/books/:id', async (req, res) => { 
+    app.delete('/books/:id', async (req, res) => {
       const id = req.params.id;
       const result = await bookCollections.deleteOne({ _id: new ObjectId(id) });
       res.send(result);
