81b001fa0b4f391f0f1a1f6bc3223fd13abe397e
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-07 18:27:47 +0600

    send data using email for myitems

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.haogd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -22,62 +19,58 @@ async function run() {
   try {
     await client.connect();
     const bookCollections = client.db("bookhouse").collection("book");
+   
 
-    app.get('/books', async (req, res) => {
+    app.get("/books", async (req, res) => {
       const query = {};
       const books = await bookCollections.find(query).toArray();
       res.send(books);
-    })
+    });
 
-    app.get('/books/:id', async (req, res) => { 
-      const book = await bookCollections.findOne({ _id: new ObjectId(req.params.id) });
+    app.get("/books/:id", async (req, res) => {
+      const book = await bookCollections.findOne({
+        _id: new ObjectId(req.params.id),
+      });
       res.send(book);
-    })
+    });
 
-    app.put('/books/:id', async (req, res) => {
+    app.put("/books/:id", async (req, res) => {
       const id = req.params.id;
       const updatedQunatity = req.body.quantity;
       const updatedSold = req.body.sold;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
-      const update = { $set: { quantity: updatedQunatity,sold:updatedSold } };
+      const update = { $set: { quantity: updatedQunatity, sold: updatedSold } };
       const result = await bookCollections.updateOne(filter, update, options);
       res.send(result);
-    })
+    });
 
-    app.delete('/books/:id', async (req, res) => {
+    app.delete("/books/:id", async (req, res) => {
       const id = req.params.id;
-      const result = await bookCollections.deleteOne({ _id: new ObjectId(id) });
+      const result = await bookCollections.deleteOne({ _id: ObjectId(id) });
       res.send(result);
-    }
-    )
+    });
 
-    app.post('/books', async (req, res) => { 
+    app.post("/books", async (req, res) => {
       const book = req.body;
       const result = await bookCollections.insertOne(book);
       res.send(result);
+    });
 
-    })
-
-    
-    
-
-  
    
+    app.get("/book", async (req, res) => {
+      const email = req.query.email;
+      const query = {email:email};
+      const items = await bookCollections.find(query).toArray();
+      res.send(items);
+    });
 
 
     console.log("Connected correctly to server");
-    
-  }
-  finally {
-
+  } finally {
   }
 }
 run().catch(console.dir);
-
-
-
-
 
 app.get("/", (req, res) => {
   res.send("Welcome To Bookhouse!!!");
