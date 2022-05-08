b6094858977aa81aae6c4fc44f791e45d83c3a9f
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-08 13:44:46 +0600

    jwt verification

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) { 
+  const authHeader = req.headers.authorization;
+  if (!authHeader) { 
+    return res.status(401).send({message:"Unauthorized"});
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden" });
+    }
+    else { 
+       
+      req.decoded = decoded;
+      next();
+    }
+  });
+  
+
+    
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.haogd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -68,11 +89,18 @@ async function run() {
     });
 
    
-    app.get("/book", async (req, res) => {
+    app.get("/book", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.user.email;
+      
       const email = req.query.email;
-      const query = {email:email};
-      const items = await bookCollections.find(query).toArray();
-      res.send(items);
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const items = await bookCollections.find(query).toArray();
+        res.send(items);
+      }
+      else { 
+       res.status(403).send({ message: "forbidden access" });
+      }
     });
 
 
